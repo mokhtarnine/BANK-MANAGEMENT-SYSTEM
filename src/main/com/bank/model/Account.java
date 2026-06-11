d76e197e36654c29e79180d5537d0c07c289b7c6
mokhtar <mokhtarfertit@gmail.com>
2026-06-11 09:46:21 +0100

    here we implement classes of mdoel

--- src/main/com/bank/model/Account.java
+++ src/main/com/bank/model/Account.java
@@ -1,31 +1,57 @@
 package  com.bank.model;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
-public abstract class Account{
-    private String accountNumber;
-    private double balance;
-    private boolean closed;
-    private  LinkedList<Transaction> transactions;
-    private  ReentrantLock lock;
-    public Account(String accountNumber, double balance,boolean closed){
+
+public abstract class Account {
+    protected  String accountNumber;
+    protected double balance;
+    protected boolean closed;
+    protected List<Transaction> transactions;
+    protected final ReentrantLock lock;
+
+    public Account(String accountNumber, double balance){
         this.accountNumber = accountNumber;
         this.balance = balance;
-        this.closed = closed;
-        transactions = new LinkedList<>();
-        lock = new ReentrantLock();
+        this.closed = false;
+        this.transactions = new LinkedList<>();
+        this.lock = new ReentrantLock();
+    }
+    public String getAccountNumber(){
+        return accountNumber;
     }
     public void deposit(double amount, Employee employee){
-        balance += amount;
+        lock.lock();
+        try{
+            balance += amount;
+        } finally{
+            lock.unlock();
+        }
     }
-    public void withdraw(double amout, Employee employee){
-        balance -= amout;
-    }
+    public abstract void withdraw(double amout, Employee employee);
     public void closeAccount(){
-        closed = true;
-    }
-    public void 
+        if (balance == 0) {
+            closed = true;
+        }
 
+    }
+    public void addTransaction(Transaction transaction){
+        transaction.add(transaction);
+    }
+    public List<Transaction> getTransaction(){
+        return  transactions;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public boolean  isClosed(){
+        return closed;
+    }
+    public ReentrantLock getLock(){
+        return lock;
+    }
+    
 
 }
