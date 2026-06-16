9e17dfa4764fe5f357fd874cfadaeb1847a9cdb6
mokhtar <mokhtarfertit@gmail.com>
2026-06-16 15:57:01 +0100

    the layer of threads it complited and connected with controller classe .

--- src/main/java/com/bank/controller/BankController.java
+++ src/main/java/com/bank/controller/BankController.java
@@ -3,6 +3,12 @@ package com.bank.controller;
 import java.util.Collection;
 import java.util.List;
 
+import com.bank.thread.AlertMonitor;
+import com.bank.thread.TransactionQueue;
+import com.bank.thread.TransactionRequest;
+import com.bank.thread.TransactionWorker;
+import com.bank.model.TransactionType;
+
 import com.bank.exception.BankException;
 import com.bank.model.Account;
 import com.bank.model.Customer;
@@ -17,9 +23,25 @@ public class BankController {
     private final AuthService authService;
     private Employee currentEmployee;
 
+    private final TransactionQueue transactionQueue;
+    private final TransactionWorker transactionWorker;
+    private final Thread workerThread;
+
+    private final AlertMonitor alertMonitor;
+    private final Thread alertThread;
+
     public BankController() {
         this.bankService = new BankService();
         this.authService = new AuthService();
+
+        this.transactionQueue = new TransactionQueue();
+        this.transactionWorker = new TransactionWorker(transactionQueue, bankService);
+        this.workerThread = new Thread(transactionWorker);
+        this.workerThread.start();
+
+        this.alertMonitor = new AlertMonitor(bankService, 100.0);
+        this.alertThread = new Thread(alertMonitor);
+        this.alertThread.start();
     }
 
     public boolean login(String username, String password) {
@@ -127,4 +149,54 @@ public class BankController {
     public List<Transaction> getTransactionHistory(String accountNumber) {
         return bankService.getTransactionHistory(accountNumber);
     }
+
+    public void requestDeposit(String accountNumber, double amount) {
+        TransactionRequest request = new TransactionRequest(
+                TransactionType.DEPOSIT,
+                accountNumber,
+                null,
+                amount,
+                currentEmployee
+        );
+
+        transactionQueue.enqueue(request);
+    }
+
+    public void requestWithdraw(String accountNumber, double amount) {
+        TransactionRequest request = new TransactionRequest(
+                TransactionType.WITHDRAW,
+                accountNumber,
+                null,
+                amount,
+                currentEmployee
+        );
+
+        transactionQueue.enqueue(request);
+    }
+    public void requestTransfer(
+            String fromAccountNumber,
+            String toAccountNumber,
+            double amount
+    ) {
+        TransactionRequest request = new TransactionRequest(
+                TransactionType.TRANSFER_OUT,
+                fromAccountNumber,
+                toAccountNumber,
+                amount,
+                currentEmployee
+        );
+
+        transactionQueue.enqueue(request);
+    }
+
+    public void shutdownThreads() {
+        // this for maybe happend problem in and waiting or or sleeping.
+        transactionWorker.stop();
+        workerThread.interrupt();
+
+        alertMonitor.stop();
+        alertThread.interrupt();
+    }
+
+
 }
